5e9c0aeff77412d54a2613a0c2ef56f443c13f60
Xi-Wang <157208340+Xi-Wang99@users.noreply.github.com>
2025-05-17 20:10:48 +0800

    Add files via upload

--- data_process/multiview_dataset.py
+++ data_process/multiview_dataset.py
@@ -6,20 +6,18 @@ import torchvision.transforms as transforms
 import os
 import argparse
 
-class train_dataset(Dataset):
-    def __init__(self, args):
 
+class train_dataset(Dataset):
+    def __init__(self, args, mode):  # mode='train' or 'valid'
         data_root = args.train_root
+        self.mode = mode
         data_root1 = data_root + '/front'
         data_root2 = data_root + '/side'
         data_root3 = data_root + '/top'
 
-        # self.cut = args.cut
         test_transform = transforms.Compose([
-            #transforms.CenterCrop((self.cut, self.cut)),
-            transforms.Resize((224, 224), interpolation=2),  #功能：重置图像分辨率为224*224。interpolation-插值方法选择，默认为PIL.Image.BILINEAR
-            transforms.ToTensor(),  #图片变成[3,224,224]的张量形式
-            #transforms.Normalize(mean=[.5, .5, .5], std=[.5, .5, .5])
+            transforms.Resize((224, 224), interpolation = 2),
+            transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
         label_str = []
@@ -29,32 +27,28 @@ class train_dataset(Dataset):
         label_str2 = []
         label_str3 = []
 
-
-        for root, dirs, files in os.walk(data_root1):  # root 表示当前正在访问的文件夹路径；dirs 表示该文件夹下的子目录名list；files 表示该文件夹下的文件list
-            files.sort() #将文件排序
+        for root, dirs, files in os.walk(data_root1):
+            files.sort()
             for file in files:
-                label_str.append(file.split('_')[0])   #只记录文件名'_'以前的内容作为标签，例如bb,bf
+                label_str.append(file.split('_')  [0])
                 img_front = test_transform(Image.open(data_root1 + '/' + file))
-                imgs_front.append(np.array(img_front))  #把front图像都放在imgs_front = []中
+                imgs_front.append(img_front)
         for root, dirs, files in os.walk(data_root2):
             files.sort()
             for file in files:
-                label_str2.append(file.split('_')[0])
+                label_str2.append(file.split('_')  [0])
                 img_side = test_transform(Image.open(data_root2 + '/' + file))
-                imgs_side.append(np.array(img_side))
+                imgs_side.append(img_side)
         for root, dirs, files in os.walk(data_root3):
             files.sort()
             for file in files:
-                label_str3.append(file.split('_')[0])
+                label_str3.append(file.split('_')  [0])
                 img_top = test_transform(Image.open(data_root3 + '/' + file))
-                imgs_top.append(np.array(img_top))
+                imgs_top.append(img_top)
 
-        #print('front label = side label', label_str == label_str2)
-        #print('front label = top label', label_str == label_str3)
-        imgs_front = torch.tensor(imgs_front)
-        imgs_side = torch.tensor(imgs_side)
-        imgs_top = torch.tensor(imgs_top)
-        #把图集合转化成张量
+        imgs_front = torch.stack(imgs_front)
+        imgs_side = torch.stack(imgs_side)
+        imgs_top = torch.stack(imgs_top)
 
         label = []
         temp = 0
@@ -63,7 +57,6 @@ class train_dataset(Dataset):
             if index == 0:
                 clas = 0
                 label.append(clas)
-
             else:
                 for j in range(index):
                     if label_str[index] == label_str[index - j - 1]:
@@ -78,26 +71,62 @@ class train_dataset(Dataset):
                     tmp = 0
                     clas = temp
                     label.append(clas)
-            #print('index', index, 'label', label[index], 'tmp', tmp)
-        label = [int(x) for x in label] #把lable里面cls都化为整数形式
-        label = torch.tensor(label)   #把lable化为张量
+            # print('index', index, 'label', label[index], 'tmp', tmp)
+        label = [int(x) for x in label]
+        label = torch.tensor(label)
+        # print('All labels are:{}'.format(label))
 
-        self.imgs_front = imgs_front
-        self.imgs_side = imgs_side
-        self.imgs_top = imgs_top
-        self.label = label
+        train_label = []
+        valid_label = []
+        train_imgs_front = []
+        valid_imgs_front = []
+        train_imgs_side = []
+        valid_imgs_side = []
+        train_imgs_top = []
+        valid_imgs_top = []
+
+
+        for i, label_value in enumerate(label):
+            if label_value <= 15:
+                train_label.append(label_value.item())
+                train_imgs_front.append(imgs_front[i])
+                train_imgs_side.append(imgs_side[i])
+                train_imgs_top.append(imgs_top[i])
+            elif 16 <= label_value <= 20:
+                valid_label.append(label_value.item())
+                valid_imgs_front.append(imgs_front[i])
+                valid_imgs_side.append(imgs_side[i])
+                valid_imgs_top.append(imgs_top[i])
+
+        # print(train_label)
+        # print(valid_label)
+        self.train_imgs_front = torch.stack(train_imgs_front)
+        self.train_imgs_side = torch.stack(train_imgs_side)
+        self.train_imgs_top = torch.stack(train_imgs_top)
+        self.train_label = torch.tensor(train_label)
+        self.valid_imgs_front = torch.stack(valid_imgs_front)
+        self.valid_imgs_side = torch.stack(valid_imgs_side)
+        self.valid_imgs_top = torch.stack(valid_imgs_top)
+        self.valid_label = torch.tensor(valid_label)
+
+
 
     def __getitem__(self, i):
-        imgs_front, imgs_side, imgs_top, label = self.imgs_front[i], self.imgs_side[i], self.imgs_top[i], self.label[i]
+        if self.mode == 'train':
+            imgs_front, imgs_side, imgs_top, label = self.train_imgs_front[i], self.train_imgs_side[i], self.train_imgs_top[i], self.train_label[i]
+        elif self.mode == 'valid':
+            imgs_front, imgs_side, imgs_top, label = self.valid_imgs_front[i], self.valid_imgs_side[i], self.valid_imgs_top[i], self.valid_label[i]
         return imgs_front, imgs_side, imgs_top, label
+
+
+
+
 
 
 class test_dataset(Dataset):
     def __init__(self,args):
         super(test_dataset, self).__init__()
         roots = args.test_root
-        # roots = '/home/liujing/dataset/MULT_dataset/center_crop_800/test/grass'
-        # roots = '/home/liujing/datasets/MULT_test'
         self.envs = args.envs
         # self.cut = args.cut
 
@@ -197,6 +226,7 @@ class test_dataset(Dataset):
                     label.append(clas)
             # print('index', index, 'label', label[index], 'tmp', tmp)
         label = [int(x) for x in label]
+        # print('test labels are:{}'.format(label))
         label = torch.tensor(label)
 
         self.imgs_front = imgs_front
@@ -207,6 +237,97 @@ class test_dataset(Dataset):
     def __getitem__(self, i):
         imgs_front, imgs_side, imgs_top, label = self.imgs_front[i], self.imgs_side[i], self.imgs_top[i], self.label[i]
         return imgs_front, imgs_side, imgs_top, label
+
+    def __len__(self):
+        return len(self.label)
+
+
+
+class All_dataset(Dataset):
+    def __init__(self, args):
+
+        data_root = args.train_root
+        data_root1 = data_root + '/front'
+        data_root2 = data_root + '/side'
+        data_root3 = data_root + '/top'
+
+        # self.cut = args.cut
+        test_transform = transforms.Compose([
+            #transforms.CenterCrop((self.cut, self.cut)),
+            transforms.Resize((224, 224), interpolation=2),  #功能：重置图像分辨率为224*224。interpolation-插值方法选择，默认为PIL.Image.BILINEAR
+            transforms.ToTensor(),  #图片变成[3,224,224]的张量形式
+            #transforms.Normalize(mean=[.5, .5, .5], std=[.5, .5, .5])
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+        label_str = []
+        imgs_front = []
+        imgs_side = []
+        imgs_top = []
+        label_str2 = []
+        label_str3 = []
+
+
+        for root, dirs, files in os.walk(data_root1):  # root 表示当前正在访问的文件夹路径；dirs 表示该文件夹下的子目录名list；files 表示该文件夹下的文件list
+            files.sort() #将文件排序
+            for file in files:
+                label_str.append(file.split('_')[0])   #只记录文件名'_'以前的内容作为标签，例如bb,bf
+                img_front = test_transform(Image.open(data_root1 + '/' + file))
+                imgs_front.append(np.array(img_front))  #把front图像都放在imgs_front = []中
+        for root, dirs, files in os.walk(data_root2):
+            files.sort()
+            for file in files:
+                label_str2.append(file.split('_')[0])
+                img_side = test_transform(Image.open(data_root2 + '/' + file))
+                imgs_side.append(np.array(img_side))
+        for root, dirs, files in os.walk(data_root3):
+            files.sort()
+            for file in files:
+                label_str3.append(file.split('_')[0])
+                img_top = test_transform(Image.open(data_root3 + '/' + file))
+                imgs_top.append(np.array(img_top))
+
+        #print('front label = side label', label_str == label_str2)
+        #print('front label = top label', label_str == label_str3)
+        imgs_front = torch.tensor(imgs_front)
+        imgs_side = torch.tensor(imgs_side)
+        imgs_top = torch.tensor(imgs_top)
+        #把图集合转化成张量
+
+        label = []
+        temp = 0
+        tmp = 0
+        for index in range(len(label_str)):
+            if index == 0:
+                clas = 0
+                label.append(clas)
+
+            else:
+                for j in range(index):
+                    if label_str[index] == label_str[index - j - 1]:
+                        clas = label[index - j - 1]
+                        label.append(clas)
+                        tmp = 0
+                        break
+                    else:
+                        tmp += 1
+                if tmp == index and tmp != 0:
+                    temp += 1
+                    tmp = 0
+                    clas = temp
+                    label.append(clas)
+            #print('index', index, 'label', label[index], 'tmp', tmp)
+        label = [int(x) for x in label] #把lable里面cls都化为整数形式
+        label = torch.tensor(label)   #把lable化为张量
+
+        self.imgs_front = imgs_front
+        self.imgs_side = imgs_side
+        self.imgs_top = imgs_top
+        self.label = label
+
+    def __getitem__(self, i):
+        imgs_front, imgs_side, imgs_top, label = self.imgs_front[i], self.imgs_side[i], self.imgs_top[i], self.label[i]
+        return imgs_front, imgs_side, imgs_top, label
+
 
 
 
